23351960a7585e33024d4863f93f371a4e20bf52
Darrell Taylor <darrell@realgo.com>
2024-04-24 16:26:51 -0600

    add faq

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -5,34 +5,78 @@
 
 <svelte:head>
 	<title>FAQ</title>
-	<meta name="description" content="About this app" />
+	<meta name="description" content="Frequently asked Questions" />
 </svelte:head>
 
 <div class="text-column">
 	<h1>FAQ</h1>
-	<article class="faq">
-    <h2>What is impedance matching and why does it matter?</h2>
-    <p>The Maximum Power Transfer theorem says that we must match the load impedance to the source impedance.</p>
-  </article>
   <article class="faq">
-    <h2>What about permits?</h2>
-    <p>Less/no permits are required since we are not connecting to the grid.</p>
-  </article>
-  <article class="faq">
-    <h2>Is this better/worse then solar thermal?</h2>
-    <p>This system has no moving parts and is easier to install.  It does require more roof area.</p>
+    <h2>What are we even doing here?</h2>
+    <p>This site teaches you how to hook some solar panels to the bottom element of your water heater.  Cheap solar power with energy storage. I did this DIY and cut my household electric use in half.</p>
   </article>
 
-  <p>
-    I put some used PV panels on my shed's roof and wired them up to the bottom element of my electric water heater. This cut my household electric use in half. Not connected to the grid so no expensive permits needed.
-  
-    There is no thermostat for the PV panels. The summer power output of the panels comes into equilibrium with the heat loss of the tank at 80-85C. I did add a 93C thermal fuse to augment the safety of the PTV value.
-  
-    I have a mixing valve on the output of the water heater to mix with cold down to about 50C. This lets you get around 2x the nameplate capacity of hot water out of the tank so there is enough to bridge a cloudy day.
-  
-  To get the full capacity of the panels, it necessary to choose a water heater element that matches the source impedance(Vmpp/Immp) of the solar panel array
-  
-  If a squirel chews through the insulation of the wires,  it would be best to full floating.  That way the wire can not short to the earth grounded frame. 
-  
-  </p>
+  <article class="faq">
+    <h2>Will this kill me?</h2>
+    <p>The main dangers are falling off the roof while installing the panels, electrocution, and fire.  I did the wiring at night when there is no possibly of a shock.  I installed all the wires in conduit to reduce fire risk. I bonded the solar mounting rails and negative side of the array to a ground rod in case of lightning. </p>
+  </article>
+
+  <article class="faq">
+    <h2>What problems have you run into?</h2>
+    <p>Squirrels chewed some of the wires and I had to replace them. An cheap ebay inline fuse holder melted.  2/5 DC solar optimizers failed.  I am no sure the optimizers were worth it, they only get you 20-30% more power. The panels are not quite steep enough to shed snow by themselves, sometime I have to clean them manually.  </p>
+  </article>
+
+	<article class="faq">
+    <h2>What is impedance matching and why does it matter?</h2>
+    <p>Maximum Power Transfer theorem says we want to match the load impedance to the source impedance.   To get the full capacity of the panels, it necessary to choose a water heater element that matches the source impedance(Vmpp/Immp) of the solar panel array.  You can also use DC solar optimizers for this, they do MPPT at the panel level.   If you have a lot of partial shading, the optimizers may be worth it.  In most cases, carefully selecting the resistance of your heater element is good enough.  There is a calculator on the simulator page to help you with this. </p>
+  </article>
+
+  <article class="faq">
+    <h2>What about permits?</h2>
+    <p>Fewer or no permits are required since there is no connection to the grid.  This is a major benefit because in my county, the permits would cost more then all the materials!  Mounting the panels on a shed instead of your  house roof can avoid the need for permits in some locales. </p>
+  </article>
+
+  <article class="faq">
+    <h2>Is this better then solar thermal?</h2>
+    <p>This system has no moving parts and is cheaper and easier to install.  It reuses your existing electric water heater.  It does require more panel area. If you want to heat the whole house solar thermal would be better.</p>
+  </article>
+
+  <article class="faq">
+    <h2>Is this better then a heat pump water heater?</h2>
+    <p>Heat pump water heaters have some downsides.  They make noise, and you have to clean the air filter regularly. Some of them beep when they need the filter cleaned and they always manage to start beeping at 3am. They cost more and are less reliable then the old fashioned electric water heater. They steal heat from the air, which is great in summer but in winter this will add to your heating bill. </p>
+  </article>
+
+  <article class="faq">
+    <h2>Why use the bottom element?</h2>
+    <p>This way you can leave the top element hooked up to city power for when there is a long stretch of limited sunshine.  In my case, the top element is only used in winter. The thermal breaker on the upper heating element will trip if the water gets really hot in the summer.  You may have to take the cover off and reset the breaker come winter. </p>
+  </article>
+
+  <article class="faq">
+    <h2>Do I need a thermostat?</h2>
+    <p> The AC thermostat built into the water heater will not last long on DC power. DC arcs more since there is no zero-crossing to extinguish the arc. If your array is not too oversized you can make do with only a mixing valve and no thermostat.  In the summer, the water will get hotter then is safe so you need a hot/cold mixing valve to prevent scalds.   This extra hot water in the tank increases the amount of energy you can store.  I have made it through three cloudy days in a row before I need to use city power to heat the water.  </p>
+  </article>
+
+  <article class="faq">
+    <h2>What is the best angle to mount the solar panels?</h2>
+    <p>If possible, face the panels due south and angle them so they are a bit steeper then your latitude in degrees.   Steeper will collect more power in winter when the days are short and the sun is low. Steeper also sheds snow/leaves better. Flat mounted panels can be used but you will have to clean them frequently.  If getting to this steep angle is going to cause a lot of mounting headaches, don't worry about it too much.  My system is about 15 degrees flatter then ideal.   </p>
+  </article>
+
+  <article class="faq">
+    <h2>How big an array do I need?</h2>
+    <p> I have 1.5kw on a standard 40 gallon water heater and it works great.  In the summer this is almost too much, the water gets very hot. In the winter I still need a bit of city power from time to time. </p>
+  </article>
+
+  <article class="faq">
+    <h2>What about trees or other shading?</h2>
+    <p>Deciduous trees are not that bad because they block light in the summer when there is too much, and allow more through in the winter when there is not enough.  If you have some evergreen trees that will block the sun for half the day, you can add more panels to compensate.  </p>
+  </article>
+
+
 </div>
+
+
+<style>
+	h2 {
+		font-size: 1.2em;
+		font-weight: 700;
+	}
+</style>
--- src/routes/instructions/+page.svelte
+++ src/routes/instructions/+page.svelte
@@ -1,6 +1,4 @@
 <script>
-
-
 </script>
 
 <svelte:head>
@@ -10,18 +8,46 @@
 
 <div class="text-column">
 	<h1>Instructions</h1>
-
 	<ol>
-		<li>Calculate/Design<li>
-		<li>Order Materials<li>
 		<li>
-		  Install Panels
-		  <ol>
-			<li>Install Roof anchors</li>
-			<li>Attach Rails to anchors</li>
-			<li>Attach Panels to rails</li>
-		  </ol>
+			Calculate/Design
+			<ol>
+				<li>Determine existing cost of power used for water heating</li>
+				<li>Design your system using the simulator</li>
+				<li>Price out the BOM and calculate payback</li>
+			</ol>
+		</li>
+		<li>Order Materials</li>
+		<ol>
+			<li>Used panels are great for this, and can be had for very cheap. </li>
+			<li>Big solar panels will ship via freight so you may have to be home to receive them </li>
+			<li>The panels are easily damaged on the back/plastic covered side. A damaged panel will limit the current of the entire array, so you may want to order a spare.  </li>
+			<li>Most of the stuff I got from ebay/craigslist/amazon</li>
+		</ol>
+		<li>
+			Install Panels
+			<ol>
+				<li>Replace the shingles first if they are in bad shape.</li>
+				<li>Install Roof anchors.</li>
+				<li>Attach Rails to anchors.</li>
+				<li>Attach Panels to rails.</li>
+				<li>Attach grounding system.</li>
+				<li>Run plastic conduit from panels to house, and metal conduit inside the house going to the water heater. </li>
+				<li>Pull wire through conduit and connect it to the shutoff switch </li>
+				<li>Lockout/Tagout shutoff switch before working inside </li>
+			</ol>
 		</li>
 		<li>Electrical</li>
-	  </ol>
+		<ol>
+			<li>Turn off power to water heater</li>
+			<li>Drain water heater</li>
+			<li>Install Mixing valve</li>
+			<li>Remove bottom element cover, take picture of wires for future reference. </li>
+			<li>Disconnect bottom element from city power and secure wires so they will not short out on anything. </li>
+			<li>Install new bottom element.</li>
+			<li>Refill water heater.</li>
+			<li>Connect bottom element to panels. (make sure there is water in there or you will burn out the elements)</li>
+			<li>Turn power back on to top element.</li>
+		</ol>
+	</ol>
 </div>
