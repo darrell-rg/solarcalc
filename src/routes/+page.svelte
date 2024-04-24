0c9be5c8952e5b558e31fa3648193e749c0052d8
Darrell Taylor <darrell@realgo.com>
2024-04-24 12:31:13 -0600

    move stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -140,18 +140,7 @@
 	<img alt="SolarShed" src="solarShed/PanelsInSun.jpg" />
 	<figcaption>Cheap and simple DIY solar hot water!</figcaption>
 </figure>
-<p>
-	I put some used PV panels on my shed's roof and wired them up to the bottom element of my electric water heater. This cut my household electric use in half. Not connected to the grid so no expensive permits needed.
 
-	There is no thermostat for the PV panels. The summer power output of the panels comes into equilibrium with the heat loss of the tank at 80-85C. I did add a 93C thermal fuse to augment the safety of the PTV value.
-
-	I have a mixing valve on the output of the water heater to mix with cold down to about 50C. This lets you get around 2x the nameplate capacity of hot water out of the tank so there is enough to bridge a cloudy day.
-
-To get the full capacity of the panels, it necessary to choose a water heater element that matches the source impedance(Vmpp/Immp) of the solar panel array
-
-If a squirel chews through the insulation of the wires,  it would be best to full floating.  That way the wire can not short to the earth grounded frame. 
-
-</p>
 
 <div class="smol-flexbox-grid">
 	<div>
--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -22,4 +22,17 @@
     <h2>Is this better/worse then solar thermal?</h2>
     <p>This system has no moving parts and is easier to install.  It does require more roof area.</p>
   </article>
+
+  <p>
+    I put some used PV panels on my shed's roof and wired them up to the bottom element of my electric water heater. This cut my household electric use in half. Not connected to the grid so no expensive permits needed.
+  
+    There is no thermostat for the PV panels. The summer power output of the panels comes into equilibrium with the heat loss of the tank at 80-85C. I did add a 93C thermal fuse to augment the safety of the PTV value.
+  
+    I have a mixing valve on the output of the water heater to mix with cold down to about 50C. This lets you get around 2x the nameplate capacity of hot water out of the tank so there is enough to bridge a cloudy day.
+  
+  To get the full capacity of the panels, it necessary to choose a water heater element that matches the source impedance(Vmpp/Immp) of the solar panel array
+  
+  If a squirel chews through the insulation of the wires,  it would be best to full floating.  That way the wire can not short to the earth grounded frame. 
+  
+  </p>
 </div>
